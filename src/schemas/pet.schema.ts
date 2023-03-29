a651208223c7f3bd848def82d0f2050622ef6be7
Yuliya_Shulgan <juliette7373@gmail.com>
2023-03-29 15:26:57 +0200

    feat: CRUD

---
+++ src/schemas/pet.schema.ts
@@ -0,0 +1,33 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type PetDocument = HydratedDocument<Pet>;
+
+@Schema()
+export class Pet {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true })
+  animalType: string;
+
+  @Prop({ required: true })
+  breed: string;
+
+  @Prop({ required: true })
+  description: string;
+
+  @Prop({ required: true })
+  age: string;
+
+  @Prop()
+  inoculations?: string;
+
+  @Prop()
+  diseases?: string;
+
+  @Prop()
+  parasites?: string;
+}
+
+export const PetSchema = SchemaFactory.createForClass(Pet);
